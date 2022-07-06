3d94376f80ffdd4531668c8742f7bd8d04c268b0
Howard Zhang <75195784+Howardzhangdqs@users.noreply.github.com>
2022-07-06 16:38:19 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const Koa = require('koa');
-const app = new Koa();
+const path = require('path');
+const express = require('express');
+const app = express();
 
-app.use(async ctx => {
-    ctx.body = 'Hello Vercel, Hi Koa2';
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+const port = 3008;
+
+app.all("*", function(req, res, next){
+	
+	res.header("Access-Control-Allow-Origin","*");
+	res.header("Access-Control-Allow-Headers","content-type");
+	res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
+	
+	next();
 });
 
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+})
 
-app.listen(3008, () => {
-    console.log('3008项目启动')
-});
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+})
